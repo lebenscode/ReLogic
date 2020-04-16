3af2dc53da16ad903907f36d1ca5d5927e4c8ba5
lebenscode <63811674+lebenscode@users.noreply.github.com>
2020-04-16 21:30:59 +0300

    Update scheme.php

--- scheme.php
+++ scheme.php
@@ -25,26 +25,6 @@
 <script src="/js/output.js?<?php v(); ?>"></script>
 </head>
 <body>
- <div class="navbar_wrap clear_fix">
-    <div class="navbar_blue_item">
-	  <a href="/">Энциклопедия РЗА</a>
-	</div>
-	<div class="navbar_item">
-	  <a href="/">Векторные диаграммы</a>
-	</div>
-	<div class="navbar_item">
-	  <a href="/scheme.php">Схемы защит</a>
-	</div>
-   <div class="navbar_item">
-	  <a href="/create">Добавить</a>
-	</div>
-   <div class="navbar_item">
-	  <a href="/help">Помощь</a>
-	</div>
-  <div class="navbar_item">
-	  <a href="http://localhost/Tools/phpMyAdmin/" target="_blank">PHPMyAdmin</a>
-	</div>
-		  </div>
 		  <div class="page_box" style="width:1300px;margin:20px auto;">
   <h2 class="page_title">
     <span class="page_title_text">
